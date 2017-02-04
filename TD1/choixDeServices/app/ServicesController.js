b2bcb8326b7c6b929df20084f3aa01cdadee5d84
ErwanGarandel <erwangarandel@gmail.com>
2017-02-04 15:52:15 +0100

    comit n°n
    Le total se désaffiche, je ne sais pas pourquoi....

--- TD1/choixDeServices/app/ServicesController.js
+++ TD1/choixDeServices/app/ServicesController.js
@@ -1,9 +1,9 @@
 /**
  * Created by Erwan on 31/01/2017.
  */
-var myApp = angular.module('myApp',[]);
+var app = angular.module('myApp',[]);
 
-myApp.controller('serviceController', function() {
+app.controller('serviceController', ["$filtre", function($filtre) {
     this.totValue = 1;
     this.totPrice = 300;
     this.services =  [
@@ -12,33 +12,36 @@ myApp.controller('serviceController', function() {
             "price": 300,
             "active": true
         }, {
-        "name": "Design",
-        "price": 400,
-        "active": false
-    }, {
-        "name": "Integration",
-        "price": 250,
-        "active": false
-    }, {
-        "name": "Formation",
-        "price": 220,
-        "active": false
+            "name": "Design",
+            "price": 400,
+            "active": false
+        }, {
+            "name": "Integration",
+            "price": 250,
+            "active": false
+        }, {
+            "name": "Formation",
+            "price": 220,
+            "active": false
     }
     ];
 
     this.total = function(){
-        var p;
-        var i =0;
-        for(var j=0; j< this.services.length ;j++){
-            if (this.services[j]["active"] == true) i++;
-            p = p + this.services[j]["price"];
+        var p = 0;
+        var i = 0;
+        for(var j=0; j<this.services.length;j++){
+            if (this.services[j]["active"] == true) {
+                i++;
+                p = p + this.services[j]["price"];
+            }
         }
         this.totValue = i;
         this.totPrice = p;
+        this.prixFinal = $filtre('currency')(this.totPrice);
     };
 
     this.toggleActive = function(){
 
-    };
-});
+    }
+}]);
 
