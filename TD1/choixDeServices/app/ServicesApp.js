9ea4283e4239d4974350b2e24266b155e539f40d
ErwanGarandel <erwangarandel@gmail.com>
2017-02-06 23:47:31 +0100

    DernierCommit
    Solution finale de l'application

--- TD1/choixDeServices/app/ServicesApp.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by Erwan on 31/01/2017.
- */
---
+++ TD1/choixDeServices/js/controller.js
@@ -0,0 +1,71 @@
+/**
+ * Created by Erwan on 06/02/2017.
+ */
+var app = angular.module("ServicesApp",[]);
+
+app.controller("ServicesController" , [ "$scope","$http","$filter", function($scope,$http,$filter) {
+    $scope.totValue = 1;
+    $scope.totPrice = 300;
+    $scope.services = [
+        {
+            "name": "Web Development",
+            "price": 300,
+            "active":true
+        },{
+            "name": "Design",
+            "price": 400,
+            "active":false
+        },{
+            "name": "Integration",
+            "price": 250,
+            "active":false
+        },{
+            "name": "Formation",
+            "price": 220,
+            "active":false
+        }
+    ];
+
+    $scope.total = function(){
+        var i = 0;
+        var p = 0;
+        for(var j=0; j<$scope.services.length ; j++){
+            if($scope.services[j]["active"]===true){
+                i++;
+                p = p + $scope.services[j]["price"];
+            }
+        }
+        $scope.totValue = i ;
+        $scope.totPrice = p;
+        $scope.prixFinal = $filter('currency')($scope.totPrice);
+    };
+
+    $scope.toggleActive = function(){
+
+    };
+
+    // GESTION DES REDUCTIONS
+    $scope.reductionValide = false;
+    $scope.codeSaisi; //ce qui est affiché par defaut, ici rien
+
+
+    $http.get('../JSON/promo.json').success(function(data) {
+        $scope.codes = data;
+    });
+
+    $scope.calculReduction = function(){
+        $scope.remise="Code promo invalide";
+        $scope.prixFinal =  $filter('currency')($scope.totPrice);
+        if($scope.codes[$scope.codeSaisi]){
+            $scope.remiseNB = $scope.codes[$scope.codeSaisi]*$scope.totPrice;
+            $scope.prixFinal = $filter('currency')($scope.totPrice - $scope.remiseNB);
+            $scope.remise = $filter('currency')($scope.remiseNB);
+        }
+    };
+
+    $scope.MontrerCalculReduction = function(){
+        $scope.reductionValide = !$scope.reductionValide;
+        $scope.calculReduction();
+    }
+
+}]);
