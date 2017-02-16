d1a2a9e95137d4de8b1b82ebdb1c681c22057437
ErwanGarandel <erwangarandel@gmail.com>
2017-02-16 15:10:52 +0100

    Mise en place exercice 2

--- TD2/GestionContact/js/gestionController.js
+++ TD2/GestionContact/js/gestionController.js
@@ -2,6 +2,27 @@
  * Created by Erwan on 14/02/2017.
  */
 var gestion = angular.module("gestionContact", []);
-gestion.controller("gestionController", function(){
+gestion.controller("gestionController", function($scope){
+    var self = this;
 
+    $scope.tableauNom=
+        [
+            {
+                "name" : "ZUCKERBERG",
+                "surname" : "mark",
+                "mail" : "mark@facebook.com"
+            },
+            {
+                "name" : "GATES",
+                "surname" : "bill",
+                "mail" : "bill@microsoft.com"
+            },
+            {
+                "name" : "JOBS",
+                "surname" : "steeve",
+                "mail" : "steeve@apple.com"
+            }
+        ];
+    //$scope.identite = $scope.tableauNom;
+    this.includedItems=[];
 });
