b2c887ea472d93ec2811f4e0f77622d3c2be8630
ErwanGarandel <erwangarandel@gmail.com>
2017-03-06 19:09:36 +0100

    realisation historique
    reste à creer les 2 bouttons du formulaire

--- TD3/testConv/app/currencyController.js
+++ TD3/testConv/app/currencyController.js
@@ -1,30 +1,21 @@
 /**
- * Created by Erwan on 28/02/2017.
+ * Created by Erwan on 06/03/2017.
  */
-angular.module('currencyApp',[]).controller("currencyController",['$http', '$sce', function($http, $sce){
+currencyApp.controller('CurrencyController',['$http','$sce', function($http,$sce) {
+
     var self = this;
     this.currencies;
+    this.what = 1;
     this.from = "EUR";
     this.to = "USD";
-    this.what = 1;
     this.result;
     this.historique = false;
-    this.histo1 = {};
+    this.histo1={};
 
-    $http.get('../app/data/currencymap.json').then(successCallback, errorCallback);
-
-    function successCallback(response, status, headers, config){
+    $http.get('app/data/currencymap.json').
+    then(function(response){
         self.currencies = response.data;
-    }
-    function errorCallback(error, status, headers, config){
-        console.log("Erreur avec le statut Http : "+error.status);
-    }
-
-    this.swap = function (){
-        var tmp = self.from;
-        self.from = self.to;
-        self.to = tmp;
-    };
+    });
 
     this.getResult = function(){
         url = $sce.trustAsResourceUrl('https://free.currencyconverterapi.com/api/v3/convert?compact=y&q='+self.from+'_'+self.to);
@@ -62,6 +53,11 @@ angular.module('currencyApp',[]).controller("currencyController",['$http', '$sce
             else
                 alert('Erreur lors de la conversion');
         });
-    };
+    }
 
+    this.swap = function(){
+        var temp = self.from;
+        self.from = self.to;
+        self.to = temp;
+    }
 }]);
