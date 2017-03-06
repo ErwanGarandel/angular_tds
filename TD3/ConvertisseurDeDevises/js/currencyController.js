7ae0bf172546ccff97585ecb919e843e50101b28
ErwanGarandel <erwangarandel@gmail.com>
2017-03-06 19:37:13 +0100

    Réalisation historique
    Reste à faire les bouttons

--- TD3/ConvertisseurDeDevises/js/currencyController.js
+++ TD3/ConvertisseurDeDevises/js/currencyController.js
@@ -9,6 +9,7 @@ angular.module('currencyApp',[]).controller("currencyController",['$http', '$sce
     this.what = 1;
     this.result;
     this.historique = false;
+    this.histo1 = {};
 
     $http.get('../app/data/currencymap.json').then(successCallback, errorCallback);
 
@@ -25,17 +26,42 @@ angular.module('currencyApp',[]).controller("currencyController",['$http', '$sce
         self.to = tmp;
     };
 
-     this.getResult = function(){
-             url = $sce.trustAsResourceUrl('https://free.currencyconverterapi.com/api/v3/convert?compact=y&q='+self.from+'_'+self.to);
-         $http.jsonp(url, {jsonCallbackParam : 'callback'}).then(function(result)
-         {
-            if(!jQuery.isEmptyObject(result.data)){
-                self.result = result.data[self.from+'_'+self.to].val*self.what;
-         }
-         else {
-                alert('Erreur survenue lors de la conversion');
-         }
-         });
-     };
+    this.getResult = function(){
+        url = $sce.trustAsResourceUrl('https://free.currencyconverterapi.com/api/v3/convert?compact=y&q='+self.from+'_'+self.to);
+        $http.jsonp(url, {jsonCallbackParam: 'callback'}).
+        then(function(result){
+            if(!jQuery.isEmptyObject(result.data)) {
+                console.log(result.data);
+                this.val = result.data[self.from + '_' + self.to].val;
+                self.result = val * self.what;
+                if(self.historique === true){
+                    console.log("interieur");
+                    var conversion={
+                        from : self.from,
+                        to : self.to,
+                        amount : function(){ return self.what*this.rate},
+                        initialAmount : function(){ return self.what*this.initialRate},
+                        delta : 0,
+                        rate : val,
+                        what : self.what,
+                        date : new Date(),
+                        update: false,
+                        initialRate : val
+                    };
+                    var key=self.from+self.to;
+                    if(self.histo1[key]){
+                        var oldConversion=self.histo1[key];
+                        oldConversion.what=self.what;
+                        conversion.delta=conversion.amount()-oldConversion.initialAmount();
+                        conversion.initialRate=oldConversion.initialRate;
+                    }
+                    conversion.update=false;
+                    self.histo1[key]=conversion;
+                }
+            }
+            else
+                alert('Erreur lors de la conversion');
+        });
+    };
 
 }]);
